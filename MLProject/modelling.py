696876498848953270e2c50d0f85deb23686e5fd
IchaAulia12 <ichaaulia720@gmail.com>
2025-05-17 23:02:11 +0700

    initial commit: MLProject with CI workflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -32,11 +32,6 @@ mlflow.set_experiment("student-passed-classifier")
 
 
 df = pd.read_csv('StudentsPerformance.csv')
-df['gender'] = df['gender'].map({'female': 0, 'male': 1})
-df['race/ethnicity'] = df['race/ethnicity'].astype('category').cat.codes
-df['parental level of education'] = df['parental level of education'].astype('category').cat.codes
-df['lunch'] = df['lunch'].map({'standard': 1, 'free/reduced': 0})
-df['test preparation course'] = df['test preparation course'].map({'completed': 1, 'none': 0})
 
 df['passed'] = ((df['math score'] >= 50) &
                 (df['reading score'] >= 50) &
